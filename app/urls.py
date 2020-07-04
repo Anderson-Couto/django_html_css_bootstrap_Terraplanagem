17dd7ad725d8d940ad3fe47a1dd9320ba7aa33a0
Anderson Couto Cardoso <andersoncouto57@gmail.com>
2020-07-03 22:49:39 -0300

    Adicionado novos templates para a aba de Orçamento e contato, assim como a adição do mapa do google para a localização da empresa. CSS atualizado

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from . import views
+from .views import index, orcamento, contato
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+    path('orcamento', orcamento, name='orcamento'),
+    path('contato', contato, name='contato'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'index.html')
+
+def orcamento(request):
+    return render(request, 'orcamento.html')
+
+def contato(request):
+    return render(request, 'contato.html')
